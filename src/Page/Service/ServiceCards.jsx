b4b02adea9819abce8d52902a66cbe140b3af617
ratulislam46 <arfanratul46@gmail.com>
2026-01-04 01:00:43 +0600

    Modify services cards and responsive

--- src/Page/Service/ServiceCards.jsx
+++ src/Page/Service/ServiceCards.jsx
@@ -6,67 +6,86 @@ const serviceCardData = [
         "_id": 1,
         "title": "Dyeing",
         "description": "Adding color to biodegradable materials",
-        "image_url": "assets/images/dyeing.png"
+        "image_url": "../../../public/image/1.png"
     },
     {
         "_id": 2,
         "title": "Cutting",
         "description": "Eco-friendly clothing items for all shapes and sizes",
-        "image_url": "assets/images/cutting.png"
+        "image_url": "../../../public/image/2.png"
     },
     {
         "_id": 3,
         "title": "Sewing",
         "description": "Vouching for the ultimate sturdiness and durability of the fabric",
-        "image_url": "assets/images/sewing.png"
+        "image_url": "../../../public/image/3.png"
     },
     {
         "_id": 4,
         "title": "Snipping of thread",
         "description": "A neat edge, a soft and smooth finish",
-        "image_url": "assets/images/snipping-thread.png"
+        "image_url": "../../../public/image/4.png"
     },
     {
         "_id": 5,
         "title": "Ironing",
         "description": "Ironing it before shipping",
-        "image_url": "assets/images/ironing.png"
+        "image_url": "../../../public/image/5.png"
     },
     {
         "_id": 6,
         "title": "Checking",
         "description": "Going through each clothing piece to ensure supreme quality standards are met",
-        "image_url": "assets/images/checking.png"
+        "image_url": "../../../public/image/6.png"
     },
     {
         "_id": 7,
         "title": "Package",
         "description": "Folding and packing with the utmost care",
-        "image_url": "assets/images/package.png"
+        "image_url": "../../../public/image/7.png"
     }
 ];
 
 const ServiceCards = () => {
+
     return (
-        <section>
+        <section className='py-24'>
             <DefaultWeight>
-                <div className='flex flex-wrap justify-center gap-4 md:gap-6'>
+                <div className='flex flex-wrap justify-center gap-6 md:gap-8 pt-10'>
                     {serviceCardData?.map((card) => (
                         <div
-                            key={card.step}
-                            className='relative w-full sm:w-[45%] md:w-[22%] px-6 py-10 border border-gray-100 shadow-sm hover:border-secondary transition-all flex flex-col items-center text-center bg-white'
+                            key={card?._id}
+                            className='group relative w-full sm:w-[45%] md:w-[22%] px-6 py-10 border border-gray-100 shadow-sm flex flex-col items-center text-center bg-white cursor-pointer overflow-visible'
                         >
-                            {/* Step Number Circle */}
-                            <div className='absolute -top-3 left-1/2 -translate-x-1/2 w-8 h-8 bg-white border rounded-full flex items-center justify-center text-xs font-bold shadow-sm'>
+
+                            <div className='absolute inset-0 overflow-hidden z-0 pointer-events-none'>
+                                <div className='absolute bottom-0 left-0 w-full h-0 bg-secondary transition-all duration-1000 ease-in-out group-hover:h-full'></div>
+                            </div>
+
+                            {/* Card id  */}
+                            <div className='absolute -top-4 left-1/2 -translate-x-1/2 z-20 w-8 h-8 bg-white border border-gray-100 rounded-full flex items-center justify-center text-xs font-bold shadow-md transition-all duration-500 
+                            group-hover:bg-primary group-hover:text-white group-hover:border-primary'>
                                 {card?._id}
                             </div>
 
-                            {/* Icon (Assuming you have image_url in your data) */}
-                            <img src={card.image_url} alt={card.title} className="w-12 h-12 mb-4 object-contain" />
+                            {/* Image */}
+                            <div className="relative z-10 mt-4">
+                                <img
+                                    src={card.image_url}
+                                    alt={card.title}
+                                    className="w-12 h-12 mb-4"
+                                />
+                            </div>
 
                             {/* Content */}
-                            <h4 className='text-lg font-bold mb-2 text-gray-800'>{card?.title}</h4>
-                            <p className='text-sm text-gray-500 leading-relaxed'>{card?.description}</p>
+                            <div className="relative z-10">
+                                <h4 className='text-lg font-bold mb-2 text-gray-800 transition-all duration-700 group-hover:text-white'>
+                                    {card?.title}
+                                </h4>
+                                <p className='text-sm text-gray-500 leading-relaxed transition-all duration-700 group-hover:text-white/90'>
+                                    {card?.description}
+                                </p>
+                            </div>
                         </div>
                     ))}
                 </div>
