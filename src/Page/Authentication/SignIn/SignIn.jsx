424ac9b2c7b12fe36d223a6ba48d45263b162a58
ratulislam46 <arfanratul46@gmail.com>
2026-01-09 00:46:59 +0600

    Update: Sign in pages include firebase auth

--- src/Page/Authentication/SignIn/SignIn.jsx
+++ src/Page/Authentication/SignIn/SignIn.jsx
@@ -8,14 +8,6 @@ import { Link } from 'react-router';
 
 // Validation schema
 const signupSchema = yup.object({
-    firstName: yup.string()
-        .required('First name is required')
-        .min(2, 'First name must be at least 2 characters')
-        .max(50, 'First name must be less than 50 characters'),
-    lastName: yup.string()
-        .required('Last name is required')
-        .min(2, 'Last name must be at least 2 characters')
-        .max(50, 'Last name must be less than 50 characters'),
     email: yup.string()
         .required('Email is required')
         .email('Please enter a valid email address'),
@@ -25,14 +17,11 @@ const signupSchema = yup.object({
         .matches(
             /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/,
             'Password must contain at least one uppercase letter, one lowercase letter, and one number'
-        ),
-    confirmPassword: yup.string()
-        .required('Please confirm your password')
-        .oneOf([yup.ref('password')], 'Passwords must match')
+        )
 }).required();
 const SignIn = () => {
 
-    const { signUp } = use(AuthContext);
+    const { signIn } = use(AuthContext);
 
     const {
         register,
@@ -47,7 +36,7 @@ const SignIn = () => {
     const onSubmit = (data) => {
         try {
             // Simulate API call
-            signUp(data.email, data.password)
+            signIn(data.email, data.password)
             console.log('Form submitted:', data);
 
             // In real app, you would make an API call here
