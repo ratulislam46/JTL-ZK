222482599621d3770a361d360f7013b6dab8808b
ratulislam46 <arfanratul46@gmail.com>
2026-01-08 22:38:26 +0600

    Connect firbase and add authProvider

---
+++ src/Context/AuthProvider.jsx
@@ -0,0 +1,45 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../Firebase/firebase.config'
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    const signUp = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    };
+
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+    };
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            console.log('CURRENT USER DATA: ', currentUser);
+            setLoading(false);
+        });
+
+        // cleanup (important)
+        return () => unsubscribe();
+    }, []);
+
+    const authInfo = {
+        signUp,
+        signIn,
+        user,
+        loading
+    }
+
+    return (
+        <AuthContext value={authInfo}>
+            {children}
+        </AuthContext>
+    );
+};
+
+export default AuthProvider;
--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,21 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDjMIfaZmVa2Cuon63y68NzxsU6PQ5g33k",
-  authDomain: "jtl-zk.firebaseapp.com",
-  projectId: "jtl-zk",
-  storageBucket: "jtl-zk.firebasestorage.app",
-  messagingSenderId: "153283858218",
-  appId: "1:153283858218:web:e09847cf07ffa4cd3b5853",
-  measurementId: "G-E1RKFX9MYB"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
